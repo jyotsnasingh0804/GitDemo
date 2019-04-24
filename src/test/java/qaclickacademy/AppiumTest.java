8e8906c1ffdadace15bde7440d278773e27b268e
jyotsnasingh0804 <jyotsnasingh0804@gmal.com>
2019-04-24 22:45:59 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void BrowseAppium()
+	{
+		System.out.println("BrowseAppium");
+	}
+	
+	@Test
+	public void elementsAppium()
+	{
+		System.out.println("elementsAppium");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowseAutomation()
+	{
+		System.out.println("BrowseAutomation");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+}
