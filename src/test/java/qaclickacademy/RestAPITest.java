f9b083533d8cc87af140e9de49e04bec2961021e
jyotsnasingh0804 <jyotsnasingh0804@gmal.com>
2019-04-27 14:52:31 +0530

    3rd Commit

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -9,6 +9,8 @@ public class RestAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira1");
 		System.out.println("postJira2");
+		System.out.println("postJira3");
+		System.out.println("postJira4");
 	}
 	@Test
 	public void deleteTwitter()
